9840814954d60df37e81c73cec911664a11cad1f
i321takeji <i.321.takeji@gmail.com>
2023-02-10 20:31:47 +0900

    Add syntax

--- src/LambdaDC/Syntax.hs
+++ src/LambdaDC/Syntax.hs
@@ -1,11 +1,104 @@
-module LambdaDC.Syntax () where
+-- |
+-- Abstract syntax
+module LambdaDC.Syntax (Value (..), Expr (..), DelimitedContext (..), SeqElem (..)) where
+
+import Prettyprinter (Doc, Pretty (..), brackets, parens, (<+>))
 
 type Name = String
 
+type Prompt = Int
+
+type Sequences = [SeqElem]
+
 data Value
-  = Var Name
-  | Lam Name Expr
+  = -- | Variable
+    Var Name
+  | -- | Lambda abstraction
+    Lam Name Expr
+  | -- | Prompt number
+    PN Prompt
+  | -- | Rest of the context
+    Seq Sequences
+  deriving (Show, Eq)
 
 data Expr
-  = Val Value
-  | App Expr Expr
+  = -- | Value
+    Val Value
+  | -- | Application
+    App Expr Expr
+  | -- | Create a new prompt
+    NewPrompt
+  | -- | Delimit the current continuation
+    PushPrompt Expr Expr
+  | -- | Back to the position `p`, capture the current continuation, and apply `f` to the delimited continuation
+    WithSubCont Expr Expr
+  | -- | ##
+    PushSubCont Expr Expr
+  deriving (Show, Eq)
+
+data DelimitedContext
+  = DHole
+  | DAppL DelimitedContext Expr
+  | DAppR Value DelimitedContext
+  | DPushPrompt DelimitedContext Expr
+  | DPushSubCont DelimitedContext Expr
+  | DWithSubContE DelimitedContext Expr
+  | DWithSubContP Prompt DelimitedContext
+  deriving (Show, Eq)
+
+data SeqElem
+  = EPN Prompt
+  | EDC DelimitedContext
+  deriving (Show, Eq)
+
+instance Pretty SeqElem where
+  pretty (EPN p) = prettyPrompt p
+  pretty (EDC dc) = pretty dc
+
+instance Pretty DelimitedContext where
+  pretty DHole = pretty "@"
+  pretty (DAppL dc e) = prettyDelimitedContextParen dc <+> prettyExprParen e
+  pretty (DAppR v dc) = parens (pretty v) <+> pretty dc
+  pretty (DPushPrompt dc e) = pretty "pushPrompt" <+> pretty dc <+> prettyExprParen e
+  pretty (DPushSubCont dc e) = pretty "pushSubCont" <+> pretty dc <+> prettyExprParen e
+  pretty (DWithSubContE dc e) = pretty "withSubCont" <+> pretty dc <+> prettyExprParen e
+  pretty (DWithSubContP p dc) = pretty "withSubCont PN#" <> pretty p <> pretty dc
+
+prettyDelimitedContextParen :: DelimitedContext -> Doc ann
+prettyDelimitedContextParen dc = case dc of
+  DAppL _ _ -> parens $ pretty dc
+  DAppR _ _ -> parens $ pretty dc
+  DPushPrompt _ _ -> parens $ pretty dc
+  DWithSubContE _ _ -> parens $ pretty dc
+  DWithSubContP _ _ -> parens $ pretty dc
+  DPushSubCont _ _ -> parens $ pretty dc
+  _ -> pretty dc
+
+lambdaChar :: Char
+lambdaChar = 'λ'
+
+prettyPrompt :: Int -> Doc ann
+prettyPrompt p = pretty "PN#" <> pretty p
+
+instance Pretty Value where
+  pretty (Var v) = pretty v
+  pretty (Lam v e) = pretty lambdaChar <> pretty v <> pretty "." <> pretty e
+  pretty (PN p) = prettyPrompt p
+  pretty (Seq sqs) = brackets $ prettyList sqs
+
+instance Pretty Expr where
+  pretty (Val v) = pretty v
+  pretty (App e1 e2) = prettyExprParen e1 <+> prettyExprParen e2
+  pretty NewPrompt = pretty "newPrompt"
+  pretty (PushPrompt e1 e2) = pretty "pushPrompt" <+> prettyExprParen e1 <+> prettyExprParen e2
+  pretty (WithSubCont e1 e2) = pretty "withSubCont" <+> prettyExprParen e1 <+> prettyExprParen e2
+  pretty (PushSubCont e1 e2) = pretty "pushSubCont" <+> prettyExprParen e1 <+> prettyExprParen e2
+
+prettyExprParen :: Expr -> Doc ann
+prettyExprParen e = case e of
+  Val (Lam _ _) -> parens $ pretty e
+  App _ _ -> parens $ pretty e
+  PushPrompt _ _ -> parens $ pretty e
+  WithSubCont _ _ -> parens $ pretty e
+  PushSubCont _ _ -> parens $ pretty e
+  _ -> pretty e
---
+++ test/Test/LambdaDC/Syntax.hs
@@ -0,0 +1,85 @@
+module Test.LambdaDC.Syntax where
+
+import LambdaDC.Syntax
+import Prettyprinter
+import Test.Tasty
+import Test.Tasty.HUnit
+
+-- | x
+example1 :: Expr
+example1 = Val $ Var "x"
+
+-- | \x.x
+example2 :: Expr
+example2 = Val $ Lam "x" (Val $ Var "x")
+
+-- | (\x.x) y
+example3 :: Expr
+example3 = App (Val $ Lam "x" $ Val $ Var "x") (Val $ Var "y")
+
+-- | (\p.pushPrompt p ((\y.withSubCont p (\k.pushSubCont k x))) newPrompt
+example4 :: Expr
+example4 =
+  App
+    ( Val
+        ( Lam
+            "p"
+            ( PushPrompt
+                (Val $ Var "p")
+                (Val (Lam "y" (WithSubCont (Val $ Var "p") (Val (Lam "k" (PushSubCont (Val $ Var "k") (Val $ Var "x")))))))
+            )
+        )
+    )
+    NewPrompt
+
+-- | (\p.(\x.x) (pushPrompt p ((\y1.\y2.\1) (withSubCont p (\k.pushSubCont k z))))) newPrompt",
+example5 :: Expr
+example5 =
+  App
+    ( Val $
+        Lam
+          "p"
+          ( App
+              (Val $ Lam "x" (Val $ Var "x"))
+              ( PushPrompt
+                  (Val $ Var "p")
+                  ( App
+                      (Val $ Lam "y1" (Val $ Lam "y2" (Val $ Var "y1")))
+                      (WithSubCont (Val $ Var "p") (Val $ Lam "k" (PushSubCont (Val $ Var "k") (Val $ Var "z"))))
+                  )
+              )
+          )
+    )
+    NewPrompt
+
+-- | (\p.(λx.x) (pushPrompt p ((\y1.\y2.y1) (withSubCont p (\k.z))))) newPrompt
+example6 :: Expr
+example6 =
+  App
+    ( Val $
+        Lam
+          "p"
+          ( App
+              (Val $ Lam "x" (Val $ Var "x"))
+              ( PushPrompt
+                  (Val $ Var "p")
+                  ( App
+                      (Val $ Lam "y1" (Val $ Lam "y2" (Val $ Var "y1")))
+                      (WithSubCont (Val $ Var "p") (Val $ Lam "k" (Val $ Var "z")))
+                  )
+              )
+          )
+    )
+    NewPrompt
+
+test_prettyTest :: TestTree
+test_prettyTest =
+  testGroup
+    "pretty-print expression"
+    [ testCase "prints Variable" $ show (pretty example1) @?= "x",
+      testCase "prints Lambda abstraction" $ show (pretty example2) @?= "λx.x",
+      testCase "prints Application" $ show (pretty example3) @?= "(λx.x) y",
+      testCase "prints DC operator 1" $ show (pretty example4) @?= "(λp.pushPrompt p (λy.withSubCont p (λk.pushSubCont k x))) newPrompt",
+      testCase "prints DC operator 2" $ show (pretty example5) @?= "(λp.(λx.x) (pushPrompt p ((λy1.λy2.y1) (withSubCont p (λk.pushSubCont k z))))) newPrompt",
+      testCase "prints DC operator 3" $ show (pretty example6) @?= "(λp.(λx.x) (pushPrompt p ((λy1.λy2.y1) (withSubCont p (λk.z))))) newPrompt"
+    ]
